e98937eaa0f0649fecdf04adac81eeb9cd139c56
Ian White <ian@impressiver.com>
2012-08-08 23:05:15 -0700

    Added a couple more dev apps. Sprint.ly recipe working... ish

--- recipes/dev.sh
+++ recipes/dev.sh
@@ -2,7 +2,9 @@
 FIREFOX_VERSION='14.0.1'
 FLASH_PLAYER_VERSION='11.3.300.268'
 SUBLIME_TEXT_2_VERSION='2.0.1'
+TEXTMATE_VERSION='1.5.11_r1635'
 DROPBOX_VERSION='1.4.12'
+ADIUM_VERSION='1.5.2'
 
 ### CLI Apps ###
 
@@ -20,7 +22,10 @@ pip install virtualenvwrapper
 if grep -q -e "^WORKON_HOME=" $HOME/.extra; then
 	echo "\$WORKON_HOME already set in '~/.extra'"
 else
-	echo -e "\n# virtualenvwrapper\nexport WORKON_HOME=~/.virtualenvs" >> $HOME/.extra
+	echo -e "\n# virtualenvwrapper" >> $HOME/.extra
+	echo "export WORKON_HOME=~/.virtualenvs" >> $HOME/.extra
+	echo "export PROJECT_HOME=~/Projects" >> $HOME/.extra
+	echo "source virtualenvwrapper_lazy.sh" >> $HOME/.extra
 fi
 mkdir -p $HOME/.virtualenvs
 source $HOME/.extra
@@ -53,13 +58,13 @@ launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.redis.plist
 #export NODE_PATH=/usr/local/lib/node_modules
 
 # RCEnvironment (http://www.rubicode.com/Software/RCEnvironment/)
-if [ ! -d $HOME/Library/PreferencePanes/RCEnvironment.prefPane ]; then
-	hdiutil attach http://www.rubicode.com/Downloads/RCEnvironment-1.4.X.dmg
-	mkdir -p $HOME/Library/PreferencePanes
-	cp -R /Volumes/RCEnvironment-1.4.X/RCEnvironment.prefPane $HOME/Library/PreferencePanes
-else
-	echo "[RCEnvironment] RCEnvironment PrefPane already installed."
-fi
+# if [ ! -d $HOME/Library/PreferencePanes/RCEnvironment.prefPane ]; then
+# 	hdiutil attach http://www.rubicode.com/Downloads/RCEnvironment-1.4.X.dmg
+# 	mkdir -p $HOME/Library/PreferencePanes
+# 	cp -R /Volumes/RCEnvironment-1.4.X/RCEnvironment.prefPane $HOME/Library/PreferencePanes
+# else
+# 	echo "[RCEnvironment] RCEnvironment PrefPane already installed"
+# fi
 
 ### Desktop Apps ###
 
@@ -70,7 +75,7 @@ if [ ! -d /Applications/Google\ Chrome.app ]; then
 	cp -R /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
 	hdiutil detach /Volumes/Google\ Chrome
 else
-	echo "[Google Chrome] Chrome is already installed."
+	echo "[Google Chrome] Chrome is already installed"
 fi
 echo "[Google Chrome] Don’t forget to install extensions: REST Console, Speed Tracer (https://chrome.google.com/webstore/category/home)"
 
@@ -80,12 +85,12 @@ if [ ! -d /Applications/Firefox.app ]; then
 	hdiutil attach http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/$FIREFOX_VERSION/mac/en-US/Firefox%20$FIREFOX_VERSION.dmg
 	cp -R /Volumes/Firefox/Firefox.app /Applications/
 	hdiutil detach /Volumes/Firefox
-	cd /tmp/
+	cd /tmp
 	curl -LO https://addons.mozilla.org/firefox/downloads/latest/1843/addon-1843-latest.xpi
 	cd -
 	/Applications/Firefox.app/Contents/MacOS/firefox -install-global-extension /tmp/addon-1843-latest.xpi
 else
-	echo "[Mozilla Firefox] Firefox is already installed."
+	echo "[Mozilla Firefox] Firefox is already installed"
 fi
 echo "[Mozilla Firefox] Don't forget to enable Firebug extensions: Eventbug, NetExport, and FBTrace (https://getfirebug.com/swarms/Firefox-14.0/)"
 
@@ -101,18 +106,43 @@ if [ ! -d /Applications/Sublime\ Text\ 2.app ]; then
 	echo "\nInstalling Sublime Text 2"
 	hdiutil attach http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20$SUBLIME_TEXT_2_VERSION.dmg
 	cp -R /Volumes/Sublime\ Text\ 2/Sublime\ Text\ 2.app /Applications/
+	ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" $HOME/bin/subl
 else
 	echo "[Sublime Text 2] Sublime Text 2 is already installed"
 fi
 
+# TextMate
+if [ ! -d /Applications/TextMate.app ]; then
+	echo "\nInstalling TextMate"
+	cd /tmp
+	curl -O http://download.macromates.com/TextMate_$TEXTMATE_VERSION.zip
+	unzip /tmp/TextMate_$TEXTMATE_VERSION.zip -d /Applications
+	cd -
+else
+	echo "[TextMate] TextMate is already installed"
+fi
+
+
 # Dropbox
 if [ ! -d /Applications/Dropbox.app ]; then
 	echo "\nInstalling Dropbox"
+	cd /tmp
 	curl -O http://dl-web.dropbox.com/u/17/b/Dropbox%20$DROPBOX_VERSION.dmg
 	hdiutil attach Dropbox%20$DROPBOX_VERSION.dmg
+	cd -
 	echo "[Dropbox] Please follow the prompts. Waiting for Dropbox installer to quit..."
 	open -W /Volumes/Dropbox\ Installer/Dropbox.app
 	hdiutil detach /Volumes/Dropbox\ Installer
 else
 	echo "[Dropbox] Dropbox is already installed"
 fi
+
+# Adium
+if [ ! -d /Applications/Adium.app ]; then
+	echo "\nInstalling Adium"
+	hdiutil attach http://iweb.dl.sourceforge.net/project/adium/Adium_$ADIUM_VERSION.dmg
+	cp -R /Volumes/Adium\ $ADIUM_VERSION/Adium.app /Applications/
+else
+	echo "[Adium] Adium is already installed"
+fi
+
--- recipes/sprintly.sh
+++ recipes/sprintly.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 DEFAULT_PROJECT_DIR=$HOME/Projects/Sprint.ly/git
+DEFAULT_DATABASE_NAME='sprintly_local'
+DEFAULT_DATABASE_USER='root'
+DEFAULT_DATABASE_PASSWORD=''
+DEFAULT_DATABASE_HOST='localhost'
+DEFAULT_DATABASE_PORT='3306'
 
 PROJECT_DIR=$DEFAULT_PROJECT_DIR
 newdir=false
 while ! $newdir
 do
-	read -e -p "Where would you like to clone the Sprint.ly Github repos? [$DEFAULT_PROJECT_DIR]: " PROJECT_DIR
+	read -e -p "Where would you like to clone the Sprint.ly Github repos? [$DEFAULT_PROJECT_DIR] " PROJECT_DIR
 	if [ "$PROJECT_DIR" = "" ]
 	then
 		PROJECT_DIR=$DEFAULT_PROJECT_DIR
@@ -32,6 +37,17 @@ cd $PROJECT_DIR
 # Ask for some settings up front
 read -p "Enter your AWS Access Key: " AWS_ACCESS_KEY
 read -p "Enter your AWS Secret: " AWS_SECRET
+read -p "Enter MySQL database name (wil be created if it doesn't exit): ['$DEFAULT_DATABASE_NAME'] " DATABASE_NAME
+read -p "Enter MySQL user: ['$DEFAULT_DATABASE_USER'] " DATABASE_USER
+read -p "Enter MySQL password: ['$DEFAULT_DATABASE_PASSWORD'] " DATABASE_PASSWORD
+read -p "Enter MySQL host: ['$DEFAULT_DATABASE_HOST'] " DATABASE_HOST
+read -p "Enter MySQL port: ['$DEFAULT_DATABASE_PORT'] " DATABASE_PORT
+
+dbname=${DATABASE_NAME:-$DEFAULT_DATABASE_NAME}
+dbuser=${DATABASE_USER:-$DEFAULT_DATABASE_USER}
+dbpass=${DATABASE_PASSWORD:-$DEFAULT_DATABASE_PASSWORD}
+dbhost=${DATABASE_HOST:-$DEFAULT_DATABASE_HOST}
+dbport=${DATABASE_PORT:-$DEFAULT_DATABASE_PORT}
 
 # Setup snowbird (Sprint.ly website)
 git clone https://github.com/sprintly/sprint.ly.git
@@ -48,26 +64,40 @@ cat >> $PROJECT_DIR/sprint.ly/snowbird/overrides.m4 << EOF
 
 define(\`__AWS_KEY__', \`$AWS_ACCESS_KEY')
 define(\`__AWS_SECRET__', \`$AWS_SECRET')
-define(\`__DATABASE_NAME__', \`sprintly_local')
-define(\`__DATABASE_USER__', \`root')
-define(\`__DATABASE_PASSWORD__', \`')
-define(\`__DATABASE_HOST__', \`localhost')
-define(\`__DATABASE_PORT__', \`3306')
+define(\`__DATABASE_NAME__', \`"$dbname"')
+define(\`__DATABASE_USER__', \`$dbuser')
+define(\`__DATABASE_PASSWORD__', \`$dbpass')
+define(\`__DATABASE_HOST__', \`$dbhost')
+define(\`__DATABASE_PORT__', \`$dbport')
 define(\`__DATABASE_ENGINE__', \`mysql')
 EOF
 
 # Give a chance to modify the override values
 vim $PROJECT_DIR/sprint.ly/snowbird/overrides.m4
+# Now get rid of the comments so m4 won't pass them on
+sed -i '.backup' '/^#.*$/d' $PROJECT_DIR/sprint.ly/snowbird/overrides.m4
+rm $PROJECT_DIR/sprint.ly/snowbird/overrides.m4.backup
 
 echo "Setting up virtual environment..."
-cd $PROJECT_DIR
-mkvirtualenv --no-site-packages
-source venv/bin/activate
+source /usr/local/share/python/virtualenvwrapper.sh
+mkvirtualenv snowbird
+workon snowbird
+
+pip install -r $PROJECT_DIR/sprint.ly/snowbird/requirements-local.txt
+
+echo "Creating database '$dbname'"
+if [ -z "$dbpass" ]; then
+	mysqladmin --user=$dbuser --password=$dbpass --host=$dbhost create $dbname
+else
+	mysqladmin --user=$dbuser --host=$dbhost create $dbname
+fi
 
 echo "Building project..."
 cd $PROJECT_DIR/sprint.ly/snowbird
 make all
-./manage.py syncdb
+./manage.py syncdb --noinput
+./manage.py migrate
+./manage.py createsuperuser
 ./manage.py runserver 127.0.0.1:8000
 echo "Sprint.ly should now be available in your browser at: http://127.0.0.1:8000"
 
